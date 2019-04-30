745d3d7ea5bb5500c6d51db493fff3864b376de8
Florian Ehmke <ehmkeflorian@gmail.com>
2019-04-30 18:53:52 +0200

    Removed unused method.

--- backend/src/main/java/com/github/florianehmke/graphqlnotes/persistence/repository/NoteRepository.java
+++ backend/src/main/java/com/github/florianehmke/graphqlnotes/persistence/repository/NoteRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long>, JpaSpecificationExecutor<Note> {
-  Collection<Note> findByAuthorId(Long authorId);
 
   Long countByAuthorId(Long authorId);
 }
