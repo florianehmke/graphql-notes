607f7395088f74afa0d57c18faec826779e56d34
Florian Ehmke <ehmkeflorian@gmail.com>
2019-05-04 13:31:46 +0200

    Keycloak integration.

---
+++ backend/src/main/java/com/github/florianehmke/graphqlnotes/configuration/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.github.florianehmke.graphqlnotes.configuration;
+
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import static org.springframework.context.annotation.ScopedProxyMode.TARGET_CLASS;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) {
+    var keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+    var authorityMapper = new SimpleAuthorityMapper();
+
+    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(authorityMapper);
+    auth.authenticationProvider(keycloakAuthenticationProvider);
+  }
+
+  @Bean
+  public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+    return new KeycloakSpringBootConfigResolver();
+  }
+
+  @Bean
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
+
+  @Bean
+  @Scope(scopeName = "request", proxyMode = TARGET_CLASS)
+  public Authentication loggedInUserAuthentication() {
+    return SecurityContextHolder.getContext().getAuthentication();
+  }
+
+  @Bean
+  @Scope(scopeName = "request", proxyMode = TARGET_CLASS)
+  public KeycloakPrincipal<KeycloakSecurityContext> loggedInUsername() {
+    var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    if (principal instanceof KeycloakPrincipal) {
+      return (KeycloakPrincipal<KeycloakSecurityContext>) principal;
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+    http.csrf().disable().authorizeRequests().antMatchers("/**").hasRole("user");
+  }
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NotesModule } from './notes/notes.module';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { environment } from '../environments/environment';
+
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+  return (): Promise<any> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await keycloak.init({
+          config: environment.keycloak,
+          initOptions: {
+            onLoad: 'login-required',
+            checkLoginIframe: false
+          },
+          bearerExcludedUrls: []
+        });
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
-    NotesModule
+    NotesModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
