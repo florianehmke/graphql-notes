8b74ef816fbcd529625eab8900a4420c6d2f6cb8
Florian Ehmke <ehmkeflorian@gmail.com>
2019-05-26 09:58:51 +0200

    Forgot how awesome graphql is for a minute.

--- backend/src/main/java/com/github/florianehmke/graphqlnotes/graphql/NoteController.java
+++ backend/src/main/java/com/github/florianehmke/graphqlnotes/graphql/NoteController.java
@@ -1,15 +1,13 @@
 package com.github.florianehmke.graphqlnotes.graphql;
 
 import com.github.florianehmke.graphqlnotes.configuration.Role;
-import com.github.florianehmke.graphqlnotes.graphql.dto.NoteDto;
-import com.github.florianehmke.graphqlnotes.graphql.mapper.NoteMapper;
 import com.github.florianehmke.graphqlnotes.graphql.parameters.AddNote;
 import com.github.florianehmke.graphqlnotes.graphql.parameters.DeleteNote;
 import com.github.florianehmke.graphqlnotes.graphql.parameters.NotesFilter;
 import com.github.florianehmke.graphqlnotes.persistence.model.Note;
-import com.github.florianehmke.graphqlnotes.persistence.model.User;
 import com.github.florianehmke.graphqlnotes.service.NoteService;
 import com.github.florianehmke.graphqlnotes.service.UserService;
+import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -19,7 +17,6 @@ import org.springframework.stereotype.Controller;
 import javax.annotation.security.RolesAllowed;
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @Controller
 @GraphQLApi
@@ -28,24 +25,23 @@ public class NoteController {
 
   private UserService userService;
   private NoteService noteService;
-  private NoteMapper noteMapper;
 
   @Autowired
-  public NoteController(UserService userService, NoteService noteService, NoteMapper noteMapper) {
+  public NoteController(UserService userService, NoteService noteService) {
     this.userService = userService;
     this.noteService = noteService;
-    this.noteMapper = noteMapper;
   }
 
   @GraphQLQuery(description = "Loads notes matching the given parameters.")
-  public Collection<NoteDto> notes(NotesFilter param) {
+  public Collection<Note> notes(NotesFilter param) {
     var filter = param == null ? NotesFilter.emptyFilter() : param;
+    return noteService.findBy(filter.getBookId(), filter.getUserId(), filter.getSearchTerm());
+  }
 
+  @GraphQLQuery
+  public boolean deletable(@GraphQLContext Note note) {
     var user = userService.currentUser();
-    return noteService.findBy(filter.getBookId(), filter.getUserId(), filter.getSearchTerm())
-        .stream()
-        .map(note -> noteMapper.mapNote(note, deletableBy(note, user)))
-        .collect(Collectors.toList());
+    return note.getCreatedBy().getId().equals(user.getId());
   }
 
   @GraphQLMutation(description = "Deletes a note identified by the given parameter.")
@@ -54,13 +50,8 @@ public class NoteController {
   }
 
   @GraphQLMutation(description = "Adds a note with the given title/content to the given book.")
-  public NoteDto addNote(@NotNull AddNote param) {
+  public Note addNote(@NotNull AddNote param) {
     var user = userService.currentUser();
-    var note = noteService.addNote(param.getBookTitle(), param.getNoteTitle(), param.getContent());
-    return noteMapper.mapNote(note, deletableBy(note, user));
-  }
-
-  private static boolean deletableBy(Note note, User user) {
-    return note.getCreatedBy().getId().equals(user.getId());
+    return noteService.addNote(param.getBookTitle(), param.getNoteTitle(), param.getContent());
   }
 }
